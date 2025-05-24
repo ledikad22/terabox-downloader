d25fd58edd421890d2932fcdf1db44676b73c15b
ledikad22 <ledikad22@gmail.com>
2025-05-24 11:30:24 +0530

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = "terabox-cache-v1";
+const assetsToCache = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedRes) => {
+      return cachedRes || fetch(event.request);
+    })
+  );
+});
